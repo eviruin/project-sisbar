fb24a44de86362ac11246e50448b6ec71676e7c3
Taofik Hidayat <146046695+evika7@users.noreply.github.com>
2024-11-21 06:34:53 +0700

    Delete js directory

--- js/about.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("about-btn").addEventListener("click", function (e) {
-    e.preventDefault();
-    const aboutSection = document.getElementById("about-section");
-    aboutSection.classList.toggle("hidden");
-  });
